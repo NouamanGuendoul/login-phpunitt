5ab68f435ab867b22671ef0f587677e8ec994653
NouamanGuendoul <113364960+NouamanGuendoul@users.noreply.github.com>
2024-03-20 20:49:39 +0100

    Add files via upload

--- userTest.php
+++ userTest.php
@@ -7,43 +7,45 @@ use Login\classes\Database;
 use Login\classes\User;
 
 class UserTest extends TestCase {
-    private $user;
+    private $username;
     private $db;
-
-    protected function setUp(): void {
-        $this->user = new User(); // Instantiate the User class
-        // You may need to set up $this->db if required for testing
-    }
 
     public function testRegisterUser(): void {
         // Test successful registration
-        $this->assertTrue($this->user->registerUser('nouaman', 'Gaamn'));
+        $this->assertTrue($this->user->registerUser('nu', 'Auro'));
     
         // Test registration with existing username (should fail)
-        $this->assertFalse($this->user->registerUser('nouaman', 'Gaamn'));
+        $this->assertFalse($this->user->registerUser('nu', 'Aura'));
     }
     
 
     public function testLoginUser(): void {
-        $this->assertTrue($this->user->loginUser('nouaman', 'F'));
         
-        $this->assertFalse($this->user->loginUser('nouaman', 'D'));
+        $this->assertTrue($this->user->loginUser('nu', 'G'));
+
         
-        $this->assertFalse($this->user->loginUser('ORR', '5'));
-    }
+        $this->assertFalse($this->user->loginUser('nu', 'T'));
+
+        
+        $this->assertFalse($this->user->loginUser('nuu', '5'));
+    }    
 
     public function testIsLoggedin(): void {
+        
         $this->assertFalse($this->user->isLoggedin());
 
+    
         $this->user->loginUser('Ark', 'Parry');
         $this->assertTrue($this->user->isLoggedin());
     }
 
-    public function testLogoutUser(): void {
-        // Assuming your User class has a logout method, adjust accordingly
-        $this->user->logout();
+    public function testLogoutUser()
+        {
+        
+            $this->username->Logout();
 
-        $isDeleted = (session_status() == PHP_SESSION_NONE && empty(session_id()));
-        $this->assertTrue($isDeleted);
-    }
+            $isDeleted = (session_status() == PHP_SESSION_NONE && empty(session_id()));
+            $this->assertTrue($isDeleted);
+        }
+      
 }
